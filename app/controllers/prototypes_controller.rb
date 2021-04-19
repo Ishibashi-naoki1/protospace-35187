868fe23bfa325311baefc3b2e1e6ec88ca913816
Ishibashi-naoki1 <81144617+Ishibashi-naoki1@users.noreply.github.com>
2021-04-19 17:33:36 +0900

    miss2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
+before_action :authenticate_user!, only: [:new, :edit, :destroy, :update, :create]
 # before_action :move_to_index, only: [:update]
 
   def index
